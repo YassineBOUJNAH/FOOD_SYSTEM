7f3edd9e8a505b9ba4d8d15815667049db7f7066
llyasstrh <llyasstrh@gmail.com>
2020-01-07 21:58:39 +0100

    test

--- src/main/java/com/mycompany/fds/MainApp.java
+++ src/main/java/com/mycompany/fds/MainApp.java
@@ -34,7 +34,7 @@ public class MainApp extends Application {
  
     public static void main(String[] args) {
         
-        System.out.println("hello");
+        System.out.println("hello friend");
 
         try (
                 Connection con =DbConnection.getConnection();
