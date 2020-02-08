7fc340c4d0318d16ea4e4552f1a859c2d8fa3510
boujnah <yassineboujnah@gmail.com>
2020-02-08 19:14:08 +0100

    Amelioration du Dashboard

--- src/main/java/com/mycompany/fds/Controller/MainFoodController.java
+++ src/main/java/com/mycompany/fds/Controller/MainFoodController.java
@@ -1,17 +1,19 @@
 package com.mycompany.fds.Controller;
 
+import com.dukescript.layouts.flexbox.FlexboxLayout;
+import com.dukescript.layouts.jfxflexbox.FlexBoxPane;
 import com.jfoenix.controls.JFXListView;
 import com.mycompany.fds.FoodController;
 import com.mycompany.fds.View.FoodCard;
-import com.mycompany.fds.api.ClientHelper;
-import com.mycompany.fds.api.DbConnection;
-import com.mycompany.fds.api.ProfHelper;
-import com.mycompany.fds.api.RepasHelper;
+import com.mycompany.fds.View.RestaurantCard;
+import com.mycompany.fds.api.*;
 import com.mycompany.fds.model.Client;
 import com.mycompany.fds.model.Prof;
 import com.mycompany.fds.model.Repas;
+import com.mycompany.fds.model.Rest;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -19,7 +21,6 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -37,6 +38,7 @@ public class MainFoodController implements Initializable {
 
     public AnchorPane paneA;
     public JFXListView listeRepas;
+    public FlexBoxPane restPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -64,7 +66,27 @@ public class MainFoodController implements Initializable {
             listeRepas.setFocusTraversable(false);
             listeRepas.getStylesheets().add("/styles/Styles.css");
             listeRepas.getStyleClass().add("listeRepas");
-            //
+
+//restaurant
+            ResultSet restDatabase = stmt.executeQuery("select * from restaurant");
+            ArrayList restList = RestHelper.getRest(restDatabase);
+            restPane.setAlignContent(FlexboxLayout.AlignContent.SPACE_AROUND);
+            restPane.setFlexDirection(FlexboxLayout.FlexDirection.ROW);
+            restList.forEach((restItem) ->
+            {
+                Rest r2 = (Rest) restItem;
+                try {
+                    AnchorPane Card1 = RestaurantCard.creat(r2.getNom(),r2.getImages(),r2.getDescription());
+                    FlexBoxPane.setGrow(Card1, 1.0f);
+                    FlexBoxPane.setMargin(Card1, new Insets(5));
+                    FlexBoxPane.setFlexBasisPercent(Card1, 10f);
+                    restPane.getChildren().add(Card1);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }});
+
 
         } catch (SQLException e) {
             e.printStackTrace();
