d2c99e1583fac21353233510488e5523cd5d4fd6
llyasstrh <ilyasstrh@gmail.com>
2020-02-09 17:06:01 +0100

    Get user info from database

---
+++ src/main/java/com/mycompany/fds/model/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.mycompany.fds.model;
+
+public class CurrentUser {
+    public static int id;
+    public static String username;
+    private static String prenom;
+    public static String name;
+    public static String email;
+    public static String password;
+    public static String photo;
+
+}
