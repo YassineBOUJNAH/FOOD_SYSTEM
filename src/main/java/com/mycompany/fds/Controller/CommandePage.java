52f3a446d373e743b717cb6105a2549049441971
llyasstrh <ilyasstrh@gmail.com>
2020-02-10 13:40:55 +0100

    Current panier

--- src/main/java/com/mycompany/fds/Controller/CommandePage.java
+++ src/main/java/com/mycompany/fds/Controller/CommandePage.java
@@ -2,32 +2,29 @@ package com.mycompany.fds.Controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import com.mycompany.fds.api.RepasHelper;
+import com.mycompany.fds.model.CurrentPanier;
 import com.mycompany.fds.model.CurrentUser;
 import com.mycompany.fds.model.Repas;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.awt.event.ActionEvent;
+import javax.sound.midi.SysexMessage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CommandePage implements Initializable {
 
-    private int totalQuantite;
-    private int prixRepas;
+    private int totalQuantite = 0;
     private int totalPrix;
-    @FXML private Label nomRepas;
     @FXML private Label idRepas;
+    @FXML private Label nomRepas;
     @FXML private Label description;
     @FXML private Label quantite;
     @FXML private Label prix;
-    @FXML private Spinner spinner;
     @FXML private Label total;
     @FXML private JFXButton addRepas;
     @FXML private JFXButton deleteRepas;
@@ -35,18 +32,13 @@ public class CommandePage implements Initializable {
     @FXML private JFXButton annuler;
     @FXML private ImageView photo;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        /* Initialiser le spinner à la valeur 0, et minimum valuer = 0 and max 10 commandes par produit
-        * */
-        SpinnerValueFactory<Integer> spinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,10 ,0);
-        this.spinner.setValueFactory(spinnerValueFactory);
-        spinner.setEditable(false); //deny la modification de la valeur
-        totalQuantite = (int) spinner.getValue();
-        total.setText(String.valueOf(totalPrix));
+
     }
     public void getFood(Repas r1){
+        //int idRepasActuel = r1.getIdRepas();
+        //int qteRepasActuel = r1.getRank();
         idRepas.setText(String.valueOf(r1.getIdRepas()));
         nomRepas.setText(r1.getNomRepas());
         description.setText(r1.getDescription());
@@ -56,21 +48,16 @@ public class CommandePage implements Initializable {
         photo.setImage(image);
     }
 
-    @FXML
-    public void calculerPrix(){
-        prixRepas = Integer.parseInt(prix.getText());
-        totalPrix = totalQuantite * prixRepas;
-    }
-
-    @FXML
-    public void validerCommande(javafx.event.ActionEvent actionEvent) {
+    public void validerCommande(ActionEvent actionEvent){
+        System.out.println("You clicke me");
         int idRepasActuel = Integer.parseInt(idRepas.getText());
-        String nomRepasActuel = nomRepas.getText();
-        int quantiteCommande = Integer.parseInt(quantite.getText());
-        System.out.println("Vous avez commandé: "+nomRepasActuel
-                            +" Quantité "+quantiteCommande);
-    }
+        int qteRepasActuel = Integer.parseInt(quantite.getText());
+        CurrentPanier.ligneCommande.put(idRepasActuel, qteRepasActuel);
+        int testQ = CurrentPanier.ligneCommande.get(idRepasActuel);
+        System.out.println(testQ);
+        System.out.println("id repas actuel : "+idRepasActuel+" avec quantité: "+qteRepasActuel);
 
+    }
 
 
 }
--- src/main/java/com/mycompany/fds/model/CurrentPanier.java
+++ src/main/java/com/mycompany/fds/model/CurrentPanier.java
@@ -1,11 +1,16 @@
 package com.mycompany.fds.model;
 
+import com.mycompany.fds.Controller.CommandePage;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class CurrentPanier {
     public static LinkedList<Repas> listeRepasCommande = new LinkedList<Repas>();
     public static HashMap<Integer, Integer> ligneCommande = new HashMap<Integer, Integer>();
+
+
+
 
 
 }
