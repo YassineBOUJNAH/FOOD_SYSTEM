81ddd289b8428945404ff131cf36ee5c22c14636
boujnah <yassineboujnah@gmail.com>
2020-02-11 18:51:09 +0100

    add search (interface)

--- src/main/java/com/mycompany/fds/Controller/CurrentTrackController.java
+++ src/main/java/com/mycompany/fds/Controller/CurrentTrackController.java
@@ -1,11 +1,15 @@
 package com.mycompany.fds.Controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CurrentTrackController implements Initializable {
+    public Label chaleurLabel;
+    public Label metreLabel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
